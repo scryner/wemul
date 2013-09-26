a539d6ab6859691510a84554f0ad99882ef6009d
scryner <scryner@gmail.com>
2013-09-26 15:47:13 +0900

    fixing command-lines

--- wemul.py
+++ wemul.py
@@ -12,32 +12,175 @@ JUSTPRINT = False
 
 # class definitions
 class NetemAdjustor:
-    def __init__(self, dst_dev, dst_dev_up):
-        self.dst_dev = dst_dev
-        self.dst_dev_up = dst_dev_up
-
-        self.haveRoot = False
-        self.nClass = 0
-
-
-    def reset(self):
-        print('RESET')
-        comm = 'tc qdisc del dev %s root handle 1:' % self.dst_dev
-
-        ret = execute(comm)
-
-        if ret is not 0:
-            print('failed comm: %s' % comm)
+    class Device:
+        def __init__(self, dev, isUpstream):
+            self.dev = dev
+            self.isUpstream = isUpstream
 
             self.haveRoot = False
             self.nClass = 0
 
-            raise Exception('RESET FAIL')
-        else:
-            print('RESET SUCCESS')
+        def reset(self):
+            print('RESET')
+            comm = 'tc qdisc del dev %s root handle 1:' % self.dev
 
-        self.haveRoot = False
-        self.nClass = 0
+            ret = execute(comm)
+
+            if ret is not 0:
+                print('failed comm: %s' % comm)
+
+                self.haveRoot = False
+                self.nClass = 0
+
+                raise Exception('RESET FAIL')
+            else:
+                print('RESET SUCCESS')
+
+            self.haveRoot = False
+            self.nClass = 0
+
+
+        def setMaxBandwidth(self, bandwidth_mbit):
+            if bandwidth_mbit == 0:
+                bandwidth_mbit = 1000
+
+            self.max_bw = bandwidth_mbit
+
+
+        def _getClassId(self):
+            if self.haveRoot:
+                self.nClass += 1
+                return '1:%d' % self.nClass
+            else:
+                r2q_val = float(self.max_bw * 1024 * 1024) / 8 / 1500 * 1.1
+                r2q_val = int(r2q_val)
+
+                comm0 = 'tc qdisc add dev %s handle 1: root htb r2q %d' % (self.dev, r2q_val)
+
+                ret = execute(comm0)
+
+                if ret is not 0:
+                    print('ADJUSTING FAIL: adding tc root')
+                    print('failed comm: %s' % comm0)
+                    return
+                else:
+                    self.haveRoot = True
+
+                self.nClass = 1
+                return '1:1'
+
+        def addExceptions(self, host, except_list):
+            for ex in except_list:
+                class_id = self._getClassId()
+
+                comm = 'tc class add dev %s parent 1: classid %s htb rate %dMbit' % (self.dev, class_id, ex['bw'])
+
+                ret = execute(comm)
+
+                if ret is not 0:
+                    print('ADJUSTING FAIL: adding tc class for exception')
+                    print('failed comm: %s' % comm)
+                    return
+
+                comm = 'tc filter add dev %s parent 1: protocol ip prio 1 u32 match ip src %s match ip dst %s/32 flowid %s' % (self.dev, ex['addr'], host, class_id)
+
+                ret = execute(comm)
+
+                if ret is not 0:
+                    print('ADJUSTING FAIL: adding tc src filter for exception')
+                    print('failed comm: %s' % comm)
+                    continue
+
+                comm = 'tc filter add dev %s parent 1: protocol ip prio 1 u32 match ip dst %s match ip src %s/32 flowid %s' % (self.dev, ex['addr'], host, class_id)
+
+                ret = execute(comm)
+
+                if ret is not 0:
+                    print('ADJUSTING FAIL: adding tc src filter for exception')
+                    print('failed comm: %s' % comm)
+                    continue
+
+                netem_opt = get_netem_opt(ex['delay'], ex['loss_str'])
+
+                if netem_opt != '':
+                    comm = 'tc qdisc add dev %s parent %s handle %d netem %s' % (self.dev, class_id, self.nClass, netem_opt)
+
+                    ret = execute(comm)
+
+                    if ret is not 0:
+                        print('ADJUSTING FAIL: adding tc netem for exception')
+                        print('failed comm: %s' % comm)
+                        continue
+
+
+        def adjust(self, host, delay_ms, bandwidth_mbit, loss_rate_str):
+            class_id = self._getClassId()
+
+            if bandwidth_mbit == 0:
+                comm1 = 'tc class add dev %s parent 1: classid %s htb rate 1000Mbit' % (self.dev, class_id)
+            else:
+                comm1 = 'tc class add dev %s parent 1: classid %s htb rate %dMbit ceil %dMbit burst 15k' % (self.dev, class_id, bandwidth_mbit, bandwidth_mbit)
+
+            ret = execute(comm1)
+
+            if ret is not 0:
+                print('failed comm: %s' % comm1)
+                raise Exception('ADJUSTING FAIL: adding tc class')
+
+            # adding filter
+            if self.isUpstream:
+                match = 'ip src'
+            else:
+                match = 'ip dst'
+
+            comm2 = 'tc filter add dev %s parent 1: protocol ip prio 1 u32 match %s %s/32 flowid %s' % (self.dev, match, host, class_id)
+
+            ret = execute(comm2)
+
+            if ret is not 0:
+                print('failed comm: %s' % comm2)
+                raise Exception('ADJUSTING FAIL: adding tc filter to match %s' % match)
+
+            # adding netem
+            netem_opt = get_netem_opt(delay_ms, loss_rate_str)
+
+            if netem_opt != '':
+                comm3 = 'tc qdisc add dev %s parent %s handle %d netem %s' % (self.dev, class_id, self.nClass, netem_opt)
+
+                ret = execute(comm3)
+
+                if ret is not 0:
+                    print('failed comm: %s' % comm3)
+                    raise Exception('ADJUSTING FAIL: adding tc netem')
+
+            # adjusting route table to mangle
+            if self.isUpstream:
+                comm = 'iptables -t mangle -A PREROUTING --source %s -j MARK --set-mark %s' % (host, self.nClass)
+            else:
+                comm = 'iptables -t mangle -A POSTROUTING --destination %s -j ACCEPT' % host
+
+            ret = execute(comm)
+
+            if ret is not 0:
+                print('failed comm: %s' % comm)
+                raise Exception('ADJUSTING FAIL: mangling to routing table')
+
+
+    def __init__(self, dev, dev_up):
+        self.downDevice = self.Device(dev, False)
+        self.upDevice = None
+
+        if dev_up != "":
+            self.upDevice = self.Device(dev_up, True)
+        else:
+            dev_up = None
+
+
+    def reset(self):
+        self.downDevice.reset()
+
+        if self.upDevice != None:
+            self.upDevice.reset()
 
         # flush iptables
         comm = 'iptables -t mangle --flush'
@@ -49,15 +192,6 @@ class NetemAdjustor:
             raise Exception('RESET FAIL')
 
 
-    def _getClassId(self):
-        if self.haveRoot:
-            self.nClass += 1
-            return '1:%d' % self.nClass
-        else:
-            self.nClass = 1
-            return '1:1'
-
-
     def adjust(self, host, up_delay_ms, down_delay_ms, up_bandwidth_mbit, down_bandwidth_mbit, loss_rate_str, unparsed_except_list):
         if loss_rate_str is "":
             loss_rate_str = "0"
@@ -66,6 +200,12 @@ class NetemAdjustor:
         print('ADJUSTING UP: delay: %dms / bandwidth: %dMbit' % (up_delay_ms, up_bandwidth_mbit))
         print('ADJUSTING DOWN: delay: %dms / bandwidth: %dMbit' % (down_delay_ms, down_bandwidth_mbit))
         print('ADJUSTING LOSS: %s%%' % loss_rate_str)
+
+        # setting max bandwidth
+        self.downDevice.setMaxBandwidth(down_bandwidth_mbit)
+        
+        if self.upDevice != None:
+            self.upDevice.setMaxBandwidth(up_bandwidth_mbit)
 
         # parsing exception list
         except_list = []
@@ -119,127 +259,16 @@ class NetemAdjustor:
 
             except_list.append(ex)
 
-        # adding root
-        if not self.haveRoot:
-            class_id = self._getClassId()
-
-            r2q_val = float(max_bw * 1024 * 1024) / 8 / 1500 * 1.1
-            r2q_val = int(r2q_val)
-
-            comm0 = 'tc qdisc add dev %s handle 1: root htb r2q %d' % (self.dst_dev, r2q_val)
-
-            ret = execute(comm0)
-
-            if ret is not 0:
-                print('ADJUSTING FAIL: adding tc root')
-                print('failed comm: %s' % comm0)
-                return
-            else:
-                self.haveRoot = True
-
             # adding exception filter which doesn't need delay
-            for ex in except_list:
-                class_id = self._getClassId()
-
-                comm = 'tc class add dev %s parent 1: classid %s htb rate %dMbit' % (self.dst_dev, class_id, ex['bw'])
-
-                ret = execute(comm)
-
-                if ret is not 0:
-                    print('ADJUSTING FAIL: adding tc class for exception')
-                    print('failed comm: %s' % comm)
-                    return
-
-                comm = 'tc filter add dev %s parent 1: protocol ip prio 1 u32 match ip src %s match ip dst %s/32 flowid %s' % (self.dst_dev, ex['addr'], host, class_id)
-
-                ret = execute(comm)
-
-                if ret is not 0:
-                    print('ADJUSTING FAIL: adding tc src filter for exception')
-                    print('failed comm: %s' % comm)
-                    continue
-
-                comm = 'tc filter add dev %s parent 1: protocol ip prio 1 u32 match ip dst %s match ip src %s/32 flowid %s' % (self.dst_dev, ex['addr'], host, class_id)
-
-                ret = execute(comm)
-
-                if ret is not 0:
-                    print('ADJUSTING FAIL: adding tc src filter for exception')
-                    print('failed comm: %s' % comm)
-                    continue
-
-                netem_opt = get_netem_opt(ex['delay'], ex['loss_str'])
-
-                if netem_opt != '':
-                    comm = 'tc qdisc add dev %s parent %s handle %d netem %s' % (self.dst_dev, class_id, self.nClass, netem_opt)
-
-                    ret = execute(comm)
-
-                    if ret is not 0:
-                        print('ADJUSTING FAIL: adding tc netem for exception')
-                        print('failed comm: %s' % comm)
-                        continue
+            self.downDevice.addExceptions(host, except_list)
 
         # adding class for upstream
-        self._adjust(self.dst_dev, host, False, down_delay_ms, down_bandwidth_mbit, loss_rate_str)
+        self.downDevice.adjust(host, down_delay_ms, down_bandwidth_mbit, loss_rate_str)
 
-        if self.dst_dev_up != '':
-            self._adjust(self.dst_dev_up, host, True, up_delay_ms, up_bandwidth_mbit, loss_rate_str)
+        if self.upDevice != None:
+            self.upDevice.adjust(host, up_delay_ms, up_bandwidth_mbit, loss_rate_str)
 
         print('ADJUSTING SUCCESS')
-
-
-    def _adjust(self, dev, host, isUpstream, delay_ms, bandwidth_mbit, loss_rate_str):
-        class_id = self._getClassId()
-
-        if bandwidth_mbit == 0:
-            comm1 = 'tc class add dev %s parent 1: classid %s htb rate 1000Mbit' % (dev, class_id)
-        else:
-            comm1 = 'tc class add dev %s parent 1: classid %s htb rate %dMbit ceil %dMbit burst 15k' % (dev, class_id, bandwidth_mbit, bandwidth_mbit)
-
-        ret = execute(comm1)
-
-        if ret is not 0:
-            print('failed comm: %s' % comm1)
-            raise Exception('ADJUSTING FAIL: adding tc class')
-
-        # adding filter
-        if isUpstream:
-            match = 'ip src'
-        else:
-            match = 'ip dst'
-
-        comm2 = 'tc filter add dev %s parent 1: protocol ip prio 1 u32 match %s %s/32 flowid %s' % (dev, match, host, class_id)
-
-        ret = execute(comm2)
-
-        if ret is not 0:
-            print('failed comm: %s' % comm2)
-            raise Exception('ADJUSTING FAIL: adding tc filter to match %s' % match)
-
-        # adding netem
-        netem_opt = get_netem_opt(delay_ms, loss_rate_str)
-
-        if netem_opt != '':
-            comm3 = 'tc qdisc add dev %s parent %s handle %d netem %s' % (dev, class_id, self.nClass, netem_opt)
-
-            ret = execute(comm3)
-
-            if ret is not 0:
-                print('failed comm: %s' % comm3)
-                raise Exception('ADJUSTING FAIL: adding tc netem')
-
-        # adjusting route table to mangle
-        if isUpstream:
-            comm = 'iptables -t mangle -A PREROUTING --source %s -j MARK --set-mark %s' % (host, self.nClass)
-        else:
-            comm = 'iptables -t mangle -A POSTROUTING --destination %s -j ACCEPT' % host
-
-        ret = execute(comm)
-
-        if ret is not 0:
-            print('failed comm: %s' % comm)
-            raise Exception('ADJUSTING FAIL: mangling to routing table')
 
 
 def get_local_ip_addr():
